9d680337823da9cba52997f1194737e0983f844d
Your Name <you@example.com>
2023-04-14 16:42:19 +0700

    sss

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ const app = express();
 
 var cron = require("node-cron");
 
-cron.schedule("*/2 * * * *", () => {
-  request("http://147.50.227.164:4000/huay", function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log("done huay");
-    } else console.log("fail huay");
-  });
-  request(
-    "http://147.50.227.164:3001/yeekee",
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log("done yeekee");
-      } else console.log("fail yeekee");
-    }
-  );
-});
+cron.schedule(
+  "*/3 * * * *",
+  () => {
+    request(
+      "http://147.50.227.164:4000/huay",
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log("done huay");
+        } else console.log("fail huay");
+      }
+    );
+    setTimeout(() => {
+      request(
+        "http://147.50.227.164:3001/yeekee",
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log("done yeekee");
+          } else console.log("fail yeekee");
+        }
+      );
+    });
+  },
+  30000
+);
+
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
